048654c05e3fe3c28cbcdbe346a09c8ac187bd28
Sergey Melnikov <39922197+MelnikovSergey@users.noreply.github.com>
2022-01-06 00:01:52 +0300

    Update index.php

--- typical-chatbot/index.php
+++ typical-chatbot/index.php
@@ -1,1 +1,18 @@
 <?php
+
+class ChatBot 
+{
+	private $botName;
+
+	public function __construct($name) {
+		$this->botName = $name;
+	}
+
+	public function typicalGreeting() {
+		return 'Hello, my name is ' . $this->botName;
+	}
+
+}
+
+$bot = new ChatBot('A virtual beauty named Olga');
+$bot->typicalGreeting();
