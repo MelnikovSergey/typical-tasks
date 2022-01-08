d744e46f42699a49290e2c2919dd85955bedf14a
unknown <sergeyandmelnikov@gmail.com>
2022-01-08 23:47:42 +0300

    update index.php, add readme.txt

--- typical-chatbot/index.php
+++ typical-chatbot/index.php
@@ -3,16 +3,18 @@
 class ChatBot 
 {
 	private $botName;
+	private $botProf;
 
-	public function __construct($name) {
+	public function __construct($name, $prof) {
 		$this->botName = $name;
+		$this->botProf = $prof;
 	}
 
 	public function typicalGreeting() {
-		return 'Hello, my name is ' . $this->botName;
+		return 'Hello, my name is ' . $this->botName . ' and my prof is ' . $this->botProf . '.';
 	}
 
 }
 
-$bot = new ChatBot('A virtual beauty named Olga');
+$bot = new ChatBot('A virtual beauty named Olga', 'funny dialog chat bot');
 $bot->typicalGreeting();
