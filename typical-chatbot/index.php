8eb77ccbc991b37358cbb5f43869da4e352f276f
unknown <sergeyandmelnikov@gmail.com>
2022-01-10 22:49:05 +0300

    update index.php

--- typical-chatbot/index.php
+++ typical-chatbot/index.php
@@ -4,6 +4,7 @@ class ChatBot
 {
 	private $botName;
 	private $botProf;
+	private $message;
 
 	public function __construct($name, $prof) {
 		$this->botName = $name;
@@ -14,7 +15,12 @@ class ChatBot
 		return 'Hello, my name is ' . $this->botName . ' and my prof is ' . $this->botProf . '.';
 	}
 
+	public function questionToTheBot($message) {
+		return 'You asked: ' . '"' . $message . '"';
+	}
+
 }
 
 $bot = new ChatBot('A virtual beauty named Olga', 'funny dialog chat bot');
 $bot->typicalGreeting();
+$bot->questionToTheBot('Как дела?');
