ce7e0269cf4427e8d8f2d9cd736cce7529b18fae
unknown <sergeyandmelnikov@gmail.com>
2022-03-04 01:59:14 +0300

    the work on the shop card is finished

--- typical-ui/e-shop-card/js/scripts.js
+++ typical-ui/e-shop-card/js/scripts.js
@@ -5,13 +5,23 @@
 
 	if(products) {
 		products.forEach(el => {
-			const imageTriggerItems = el.querySelectorAll('.image-trigger__item');
-			const imageDots = el.querySelector('.image-dots');
+			let currentProduct = el;
+			const imageTriggerItems = currentProduct.querySelectorAll('.image-trigger__item');
+			const imageDots = currentProduct.querySelector('.image-dots');
 
 			if(imageTriggerItems.length > 1) {
 				imageTriggerItems.forEach((el, index) => {
 					el.setAttribute('data-index', index);
-					imageDots.innerHTML += '<li class="image-dots__item" data-index="${index}"></li>';
+					imageDots.innerHTML += `<li class="image-dots__item ${index == 0 ? 'image-dots__item--active' : ''}" data-index="${index}"></li>`;
+					el.addEventListener('mouseenter', (e) => {
+						currentProduct.querySelectorAll('.image-dots__item').forEach((el => {el.classList.remove('image-dots__item--active')}));
+						currentProduct.querySelector(`.image-dots__item[data-index="${e.currentTarget.dataset.index}"]`).classList.add('image-dots__item--active');
+					});
+
+					el.addEventListener('mouseleave', (e) => {
+						currentProduct.querySelectorAll('.image-dots__item').forEach((el => {el.classList.remove('image-dots__item--active')}));
+						currentProduct.querySelector(`.image-dots__item[data-index="0"]`).classList.add('image-dots__item--active');
+					});	
 				});
 			}
 		});
