32e384dd798c4033fbf75060382949889521df8d
unknown <sergeyandmelnikov@gmail.com>
2022-03-03 23:57:39 +0300

    add new styles and js

--- typical-ui/e-shop-card/js/scripts.js
+++ typical-ui/e-shop-card/js/scripts.js
@@ -1,3 +1,20 @@
-$(document).ready(function() {
-	//...
-});
+(function(id) {
+	'use strict';
+
+	const products = document.querySelectorAll('.product');
+
+	if(products) {
+		products.forEach(el => {
+			const imageTriggerItems = el.querySelectorAll('.image-trigger__item');
+			const imageDots = el.querySelector('.image-dots');
+
+			if(imageTriggerItems.length > 1) {
+				imageTriggerItems.forEach((el, index) => {
+					el.setAttribute('data-index', index);
+					imageDots.innerHTML += '<li class="image-dots__item" data-index="${index}"></li>';
+				});
+			}
+		});
+	}
+
+}(document));
