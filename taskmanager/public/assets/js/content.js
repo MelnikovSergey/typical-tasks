0e360eb139de2e3c6e532f0a277aa2b39f545e0c
unknown <sergeyandmelnikov@gmail.com>
2022-07-12 00:45:30 +0300

    added a little js

--- taskmanager/public/assets/js/content.js
+++ taskmanager/public/assets/js/content.js
@@ -1,1 +1,31 @@
-// App
+(function(){
+
+	function createApp(elem) {
+
+	 //...
+
+	}
+
+	function createTaskItemForm() {
+
+		var form = document.createElement('form');
+		var input = document.createElement('input');
+		var button = document.createElement('button'); 
+		var buttonWrapper = document.createElement('div');
+
+		buttonWrapper.classList.add('btn__wrapper');
+		
+		form.append(input);
+		form.append(buttonWrapper);
+		buttonWrapper.append(button);
+
+		return {
+			form,
+			input,
+			button
+		}	
+
+	}
+
+});
+
